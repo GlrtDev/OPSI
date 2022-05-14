f24f6eda3c8e62de9ea6830b10e6b0866fa6e41e
GlrtDev <54836047+GlrtDev@users.noreply.github.com>
2022-05-14 14:30:15 +0200

    fix adaptative filter

--- modules/adaptiveFilter.py
+++ modules/adaptiveFilter.py
@@ -1,21 +1,31 @@
 import padasip as pa
-
+import numpy as np
 """
 https://matousc89.github.io/padasip/sources/filters/lms.html
 """
 
+# we pass noise sample to predict noise and then substract it from the signal
+# n is the size (number of taps) of the filter.
 
 class AdaptiveFilterLMS:
     @staticmethod
-    def denoise(x, mu, d):
+    def denoise(x, mu, d, n=2):
         # n - length of filter
         # mu - learning rate
         # w - initial weights
-        f = pa.filters.FilterLMS(n=len(x[0]), mu=mu, w="random")
+        # f = pa.filters.FilterLMS(n=len(x[0]), mu=mu, w="random")
+        
+        newX = pa.input_from_history(x[:,-1],n)
+        f = pa.filters.FilterLMS(n=n, mu=mu, w="random")
 
-        y, e, w = f.run(d, x)
+        d = d[:-(n-1),-1]
 
-        return y
+        y, e, w = f.run(d, newX)
+
+        signalDenoised = np.copy(x)
+        signalDenoised[:,-1] -= np.append(y,[0 for i in range(n-1)])
+
+        return signalDenoised
 
 
 
